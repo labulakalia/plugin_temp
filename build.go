3d08ee0abb6cb7d809f64b8b547e8bb0e1770c97
labulakalia <labulakalia@gmail.com>
2025-01-11 12:08:11 +0800

    remove plugin config proto

--- build.go
+++ build.go
@@ -6,8 +6,6 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/labulakalia/plugin_temp/proto"
-
 	"log/slog"
 	"os"
 	"os/exec"
@@ -15,23 +13,35 @@ import (
 	"github.com/BurntSushi/toml"
 )
 
+type PluginConfig struct {
+	Id           string   `toml:"id"`
+	Name         string   `toml:"name"`
+	Desc         string   `toml:"desc"`
+	Author       []string `toml:"author"`
+	Version      string   `toml:"version"`
+	ReleasedTime string   `toml:"released_time"`
+	Url          string   `toml:"url"`
+	Icon         string   `toml:"icon"`
+	Changelog    []string `toml:"changelog"`
+}
+
 func main() {
 	pluginTomlFile := "plugin.toml"
-	driverPluginConfig := &proto.DriverPluginConfig{}
-	_, err := toml.DecodeFile(pluginTomlFile, driverPluginConfig)
+	pluginConfig := &PluginConfig{}
+	_, err := toml.DecodeFile(pluginTomlFile, pluginConfig)
 	if err != nil {
 		slog.Error("decode plugin.toml failed", "err", err)
 		os.Exit(1)
 	}
-	if len(driverPluginConfig.Icon) > 0 {
-		_, err = os.Stat(driverPluginConfig.Icon)
+	if len(pluginConfig.Icon) > 0 {
+		_, err = os.Stat(pluginConfig.Icon)
 		if err != nil {
 			slog.Error("decode plugin.toml failed", "err", err)
 			os.Exit(1)
 		}
 	}
 	slog.Info("driver plugin start building...")
-	buildWasmFile := fmt.Sprintf("%s.wasm", driverPluginConfig.Id)
+	buildWasmFile := fmt.Sprintf("%s.wasm", pluginConfig.Id)
 	defer os.Remove(buildWasmFile)
 	buildCmd := fmt.Sprintf("GOOS=wasip1 GOARCH=wasm go1.24rc1 build -buildmode=c-shared -o %s export.go", buildWasmFile)
 	cmd := exec.Command("bash", "-c", buildCmd)
@@ -42,8 +52,8 @@ func main() {
 		slog.Error("exec command failed", "err", err)
 		os.Exit(1)
 	}
-	outCompressFile := fmt.Sprintf("%s.zip", driverPluginConfig.Id)
-	outFile, err := os.Create(fmt.Sprintf("%s.zip", driverPluginConfig.Id))
+	outCompressFile := fmt.Sprintf("%s.zip", pluginConfig.Id)
+	outFile, err := os.Create(fmt.Sprintf("%s.zip", pluginConfig.Id))
 	if err != nil {
 		slog.Error("create file failed", "file", outCompressFile, "err", err)
 		os.Exit(1)
@@ -57,8 +67,8 @@ func main() {
 
 	fileList := []string{
 		pluginTomlFile,
-		fmt.Sprintf("%s.wasm", driverPluginConfig.Id),
-		driverPluginConfig.Icon,
+		fmt.Sprintf("%s.wasm", pluginConfig.Id),
+		pluginConfig.Icon,
 	}
 
 	for _, file := range fileList {
